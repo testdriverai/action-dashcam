108a3143537cb19c5a1f449f006f3877c9c42236
Mohamed Hegazy <mhegazy@thebrowser.company>
2025-06-09 15:45:45 -0700

    Add option to set cli-version

--- src/windows/Install-DashcamCLI.ps1
+++ src/windows/Install-DashcamCLI.ps1
@@ -1,8 +1,15 @@
+param (
+  [Parameter(Mandatory = $true)]
+  [string]$Version
+)
+
 $global:ErrorActionPreference = "Stop"
 $global:ProgressPreference = "SilentlyContinue"
 Set-StrictMode -Version Latest
 
-& npm install --location=global dashcam
+Write-Host "Installing Dashcam CLI $Version..."
+
+& npm install --location=global dashcam@$Version
 
 Write-Host "Verifying Dashcam CLI Install..."
 & dashcam --help
